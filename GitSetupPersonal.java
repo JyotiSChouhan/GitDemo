af338480a27a83bed32be08a3ce28d77b61cb28e
JyotiSChouhan <jyotisinghchouhan772@gmail.com>
2025-10-24 00:57:05 +0530

    Update GitSetupPersonal.java

--- GitSetupPersonal.java
+++ GitSetupPersonal.java
@@ -3,5 +3,7 @@ class GitSetupPersonal
 	public static void main(String ar[])
 	{
 		System.out.println("Git Set-up Personal");
+		System.out.println("Git Set-up Personal");
+		System.out.println("Git Set-up Personal");
 	}
 }
